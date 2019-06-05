682d5a69e4d26d4c38315809b16871f740e45dcc
Jose Peralta Bonadiez <joseperalta.b39@gmail.com>
2019-06-05 01:27:47 -0500

    Cambios visuales a estandar NavBar

--- app/src/main/java/com/codeworks/appstudent/MainActivity.java
+++ app/src/main/java/com/codeworks/appstudent/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        /*int id = item.getItemId();
 
         if (id == R.id.nav_home) {
             // Handle the camera action
@@ -89,7 +89,7 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
-        }
+        }*/
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
